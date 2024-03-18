36db38c3e8847f2789b5c3691d03d8003bfbfa98
Zoltan Fabian <fszotyi@yahoo.com>
2024-03-18 11:54:26 +0200

    Attempt#1 to make the package to handle modulepaths.

--- src/Commands/LangscannerCommand.php
+++ src/Commands/LangscannerCommand.php
@@ -12,46 +12,75 @@ use Illuminate\Filesystem\Filesystem;
 
 class LangscannerCommand extends Command
 {
-    protected $signature = 'langscanner {language?}';
+    protected $signature = 'langscanner {language?} {--path= : The path to scan for translation keys}';
     protected $description = "Updates translation files with missing translation keys.";
 
     public function handle(Filesystem $filesystem): void
     {
-        if ($this->argument('language')) {
-            $languages = new Languages([$this->argument('language')]);
+        $language = $this->argument('language');
+        $modulePath = $this->option('path');
+
+        if ($modulePath) {
+            $outputPath = $modulePath . '/resources/lang/';
         } else {
-            $languages = Languages::fromPath(config('langscanner.lang_dir_path'), $filesystem);
+            $outputPath = config('langscanner.lang_dir_path') . '/';
         }
 
-        foreach ($languages->all() as $language) {
-            $fileTranslations = new CachedFileTranslations(
-                new FileTranslations(['language' => $language])
-            );
+        $languages = $this->getLanguages($language, $outputPath, $filesystem);
 
-            $missingTranslations = new MissingTranslations(
-                new RequiredTranslations(config('langscanner')),
-                $fileTranslations
-            );
-
-            $fileTranslations->update(
-                // sets translation values to empty string
-                array_fill_keys(
-                    array_keys($missingTranslations->all()),
-                    ''
-                )
-            );
-
-            // Render table
-            $this->comment(PHP_EOL);
-            $this->comment(strtoupper($language) . " missing translations:");
-
-            $rows = [];
-
-            foreach ($missingTranslations->all() as $key => $path) {
-                $rows[] = [$key, $path];
-            }
-
-            $this->table(["Key", "Path"], $rows);
+        foreach ($languages as $language) {
+            $this->processLanguage($language, $outputPath, $filesystem);
         }
+    }
+
+    protected function getLanguages($language, $outputPath, $filesystem): array
+    {
+        if ($language) {
+            return [$language];
+        } else {
+            // If no specific language is provided, determine available languages from the outputPath
+            return Languages::fromPath($outputPath, $filesystem)->all();
+        }
+    }
+
+    protected function processLanguage($language, $outputPath, $filesystem): void
+    {
+        $fileTranslations = new CachedFileTranslations(
+            new FileTranslations([
+                'language' => $language,
+                'disk' => $filesystem,
+                'rootPath' => $outputPath,
+            ])
+        );
+
+        $missingTranslations = new MissingTranslations(
+            new RequiredTranslations(config('langscanner')),
+            $fileTranslations
+        );
+
+        $fileTranslations->update(
+            // Sets translation values to empty string
+            array_fill_keys(
+                array_keys($missingTranslations->all()),
+                ''
+            )
+        );
+
+        // Render table with missing translations
+        $this->renderMissingTranslationsTable($language, $missingTranslations->all());
+    }
+
+    protected function renderMissingTranslationsTable($language, array $missingTranslations): void
+    {
+        $this->comment(PHP_EOL);
+        $this->comment(strtoupper($language) . " missing translations:");
+
+        $rows = [];
+
+        foreach ($missingTranslations as $key => $path) {
+            $rows[] = [$key, $path];
+        }
+
+        $this->table(["Key", "Path"], $rows);
     }
 }
